d63d8257327670ec93cb0db9d5c90421b76ff819
Bobby R. Stephens III <bobbyiii@outlook.com>
2024-03-02 17:01:13 -0500

    Logo??

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,34 +1,52 @@
 import React from "react";
-import "./navbar.css";
-import {Button} from "@nextui-org/react";
+// import "./navbar.css";
+import { Button } from "@nextui-org/react";
+import Hyperlapse_W from "../assets/Hyperlapse_W.png";
 
 const Navbar: React.FC = () => {
-    return (
-        <div className="navbar">
-            <ul>
-                <li>
-                    <a href="https://www.instagram.com/hyprlpse.him/">Instagram</a>
-                </li>
-                <li>
-                    <a href="/projects">Projects</a>
-                </li>
-                <li>
-                    <a href="/about">About Me</a>
-                </li>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <button>
-                    <img
-                        src="https://raw.githubusercontent.com/Bobbyiii21/hyperlapse-one/main/assets/HyperlapseWatermarkG_.png"
-                        alt="Image"
-                        className="navbar-image"
-                    />
-                    <a href="/"/>
-                </button>
-            </ul>
-        </div>
-);
+  return (
+    <div className="fixed w-full top-0 bg-black text-white p-[10px]">
+      <ul className="list-none m-0 p-0 overflow-hidden">
+        <li className="float-right">
+          <a
+            className="block text-white text-center px-[14px] py-[16px] no-underline hover:bg-[#111] rounded-[10px]"
+            href="https://www.instagram.com/hyprlpse.him/"
+          >
+            Instagram
+          </a>
+        </li>
+        <li className="float-right">
+          <a
+            className="block text-white text-center px-[14px] py-[16px] no-underline hover:bg-[#111] rounded-[10px]"
+            href="/projects"
+          >
+            Projects
+          </a>
+        </li>
+        <li className="float-right">
+          <a
+            className="block text-white text-center px-[14px] py-[16px] no-underline hover:bg-[#111] rounded-[10px]"
+            href="/about"
+          >
+            About Me
+          </a>
+        </li>
+        <li className="float-right">
+          <a
+            className="block text-white text-center px-[14px] py-[16px] no-underline hover:bg-[#111] rounded-[10px]"
+            href="/"
+          >
+            Home
+          </a>
+        </li>
+        <img
+          src={Hyperlapse_W}
+          alt="Image"
+          className="w-[50px] h-[50px] float-left"
+        />
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;

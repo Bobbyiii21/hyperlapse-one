f9a8eaad7364ba0299f02b9a68e3fe057d07c194
Bobby R. Stephens III <bobbyiii@outlook.com>
2024-03-03 00:29:26 -0500

    last deploy broke

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from "@nextui-org/react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-lg absolute inset-x-0 transform text-white text-center">
+    <footer className="text-lg absolute inset-x-0 transform text-default-500 text-center">
       <p>©{new Date().getFullYear()} Hyperlapse</p>
       <p>Designed by Bobby Stephens in Atlanta, Georgia</p>
     </footer>
